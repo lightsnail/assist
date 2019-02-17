559744c2763865eae0ad2bd4515768959525c722
LightSnail <492323595@qq.com>
2019-02-17 23:06:42 +0800

    临时 对接了 百度唤醒接口， 还未进行代码重构

--- app/src/main/java/com/warm/tech/lightsnail/MainActivity.java
+++ app/src/main/java/com/warm/tech/lightsnail/MainActivity.java
@@ -2,21 +2,37 @@ package com.warm.tech.lightsnail;
 
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
+import com.baidu.aip.asrwakeup3.core.inputstream.InFileStream;
 import com.warm.tech.float_manager.FloatViewService;
 import com.warm.tech.float_manager.GlobleFloatManager;
 
 
 public class MainActivity extends AppCompatActivity {
-    private  FloatViewService mFloatViewService;
-    private ServiceConnection mServiceConnection;
+    private BaiDuASR mBaiduASR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //
-         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        //浮动窗口单例，目前把窗口暂时放在service里面
         GlobleFloatManager.GetInstance(this,this) ;
 
+        //尝试测试百度代码
+        mBaiduASR = new BaiDuASR(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mBaiduASR.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBaiduASR.onDestroy();
     }
 }
